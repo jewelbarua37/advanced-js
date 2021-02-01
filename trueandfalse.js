92e02f22277c7353123ad42c600205ea35bb459c
Jewel Barua <jewelbarua37@gmail.com>
2021-02-01 23:43:05 +0600

    true and false

---
+++ trueandfalse.js
@@ -0,0 +1,13 @@
+// const age = 0 will return false;
+// const name = ""if string is empty will return false and if string contains empty space will retrun true
+// const name = null; ====> false
+// if(age){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+
+let name = "olaiman Khan";
+if(name)
